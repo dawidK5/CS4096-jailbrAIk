47289d2cd3834fb702490b537a13955529710948
MrBaconMajster <48122592+MrBaconMajster@users.noreply.github.com>
2023-11-29 20:18:35 +0000

    idle prefab added

--- Assets/Scripts/Behaviour Tree/AI/SpawnPoint.cs
+++ Assets/Scripts/Behaviour Tree/AI/SpawnPoint.cs
@@ -6,6 +6,7 @@ public class SpawnPoint : MonoBehaviour
 {
     public Transform transformRef;
     public Vector3 spawnPosition;
+
     // Start is called before the first frame update
     void Start()
     {
